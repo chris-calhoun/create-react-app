27577ee7ace499031c5a18190c6c7b5900d8ec1b
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-31 01:01:31 -0500

    update app.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,23 +1,15 @@
+import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>INSIDE APP COMPONENT</h2>
+        <button className="btn btn-info">I am a button</button>
+      </div>
+    );
+  }
 }
 
 export default App;
